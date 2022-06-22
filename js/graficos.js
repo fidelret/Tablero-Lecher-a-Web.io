6ad69848be52487f93afa012e9fb283f46c9f15c
Felix Martin Retes <martinretes@gmail.com>
2022-06-21 23:35:39 -0300

    .

--- js/graficos.js
+++ js/graficos.js
@@ -1,9 +1,9 @@
 
-const mar22Prelitro = document.getElementById("PRefPagProd").getContext("2d");
-const grafmar22Prelitro = new Chart(mar22Prelitro, {
+const precio_litro = document.getElementById("PRefPagProd").getContext("2d");
+const graf_precio_litro = new Chart(precio_litro, {
 type: 'bar',
 data: {
-labels: ['abr 21', 'may 21','jun 21', 'jul 21', 'ago 21', 'sep 21', 'oct 21', 'nov 21', 'dic 21', 'ene 22', 'feb 22', 'mar 22' ],
+labels: ['abr 21', 'may 21','jun 21', 'jul 21', 'ago 21', 'sep 21', 'oct 21', 'nov 21', 'dic 21', 'ene 22', 'feb 22', 'mar 22'],
 datasets: [{
     label: 'Valores en pesos $',
     data: [28.40, 30.15, 31.69, 32.73, 33.42, 33.90, 34.14, 35.52, 36.15, 36.58, 38.43, 41.12],
@@ -31,5 +31,15 @@ scales: {
 
 
 
+
+
+/*
+console.log(graf_precio_litro.data.labels);
+console.log(graf_precio_litro.data.datasets[0].data);
+
+graf_precio_litro.data.labels.push(['abr 21', 'may 21','jun 21', 'jul 21', 'ago 21', 'sep 21', 'oct 21', 'nov 21', 'dic 21', 'ene 22', 'feb 22', 'mar 22' ]);
+graf_precio_litro.data.datasets[0].data.push([28.40, 30.15, 31.69, 32.73, 33.42, 33.90, 34.14, 35.52, 36.15, 36.58, 38.43, 41.12]);
+
+*/
 
 
