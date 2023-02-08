a8f712c5fab27fede49db60275e0652e347131f2
fidelret <97366867+fidelret@users.noreply.github.com>
2023-02-08 14:17:02 -0300

    dic_22

--- js/graficos.js
+++ js/graficos.js
@@ -3,10 +3,10 @@ const precio_litro = document.getElementById("PRefPagProd").getContext("2d");
 const graf_precio_litro = new Chart(precio_litro, {
 type: 'bar',
 data: {
-labels: ['Nov 21','Dic 21','Ene 22','Feb 22','Mar 22','Abr 22','May 22','Jun 22','Jul 22','Ago 22','Sep 22','Oct 22','Nov 22'],
+labels: ['Dic 21','Ene 22','Feb 22','Mar 22','Abr 22','May 22','Jun 22','Jul 22','Ago 22','Sep 22','Oct 22','Nov 22','Dic 22'],
 datasets: [{
     label: 'Valores en pesos $',
-    data: [35.52,35.15,36.58,38.43,41.12,44.31,46.87,49.21,52.06,54.34,57.03,59.88,63.17],
+    data: [35.15,36.58,38.43,41.12,44.31,46.87,49.21,52.06,54.34,57.03,59.88,63.17,67.76],
     backgroundColor: [
         'rgba(0,55,203,0.5)',
     ],
@@ -34,10 +34,10 @@ const produccion_mensual = document.getElementById("lech_crud").getContext("2d")
 const graf_produccion_mensual = new Chart(produccion_mensual, {
 type: 'bar',
 data: {
-labels: ['Nov 21','Dic 21','Ene 22','Feb 22','Mar 22','Abr 22','May 22','Jun 22','Jul 22','Ago 22','Sep 22','Oct 22','Nov 22'],
+labels: ['Dic 21','Ene 22','Feb 22','Mar 22','Abr 22','May 22','Jun 22','Jul 22','Ago 22','Sep 22','Oct 22','Nov 22','Dic 22'],
 datasets: [{
     label: 'Producción mensual de leche cruda en millones de litros',
-    data: [212.12,203.04,179.11,155.57,169.09,164.54,177.47,174.59,187.25,204.09,208.46,217.48,204.07],
+    data: [203.04,179.11,155.57,169.09,164.54,177.47,174.59,187.25,204.09,208.46,217.48,204.07,201.86],
     backgroundColor: [
         'rgba(255,120,0,0.5)',
     ],
@@ -63,10 +63,10 @@ const solidos_utiles = document.getElementById("graf_solidos_Utiles").getContext
 const graf_solidos_utiles = new Chart(solidos_utiles, {
 type: 'line',
 data: {
-labels: ['Nov 21','Dic 21','Ene 22','Feb 22','Mar 22','Abr 22','May 22','Jun 22','Jul 22','Ago 22','Sep 22','Oct 22','Nov 22'],
+labels: ['Dic 21','Ene 22','Feb 22','Mar 22','Abr 22','May 22','Jun 22','Jul 22','Ago 22','Sep 22','Oct 22','Nov 22','Dic 22'],
 datasets: [{
     label: '% Solidos Útiles',
-    data: [7.02,6.96,6.96,7.09,7.26,7.36,7.43,7.42,7.36,7.26,7.20,7.18,7.11],
+    data: [6.96,6.96,7.09,7.26,7.36,7.43,7.42,7.36,7.26,7.20,7.18,7.11,7.00],
     backgroundColor: [
         'rgba(255,173,0,0.5)',
     ],
